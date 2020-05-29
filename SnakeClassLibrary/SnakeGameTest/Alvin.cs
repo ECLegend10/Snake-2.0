041b30649d42eb329cdf4c2a7e2c0549679ed4ea
ELWIN <elwinchankw@gmail.com>
2020-05-30 00:38:08 +0800

    Create new projects for class library and unit tests
    SnakeClassLibrary project  - Class library
    SnakeGameTest project  - Unit tests

---
+++ SnakeClassLibrary/SnakeGameTest/Alvin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SnakeClassLibrary;
+
+namespace SnakeGameTest
+{
+    [TestClass]
+    public class Alvin
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
